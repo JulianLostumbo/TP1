991aa6384c26a59057aba08f3581bfa5b85cfa90
JulianLostumbo <62869153+JulianLostumbo@users.noreply.github.com>
2020-07-09 21:56:22 -0300

    ReportViewer agregado al proyecto

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace TP1_Reportes
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
     }
 }
